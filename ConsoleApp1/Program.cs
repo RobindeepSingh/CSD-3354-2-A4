19c9f2e0825635de0c1ef3713cda859a7ae2f62b
visualstudio <visualstudio@122-PC31>
2019-03-27 11:47:10 -0400

    11:45 commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -18,8 +19,9 @@ namespace ConsoleApp1
         }
         static async void Download()
         {
-            await Network.Download();
-            Console.WriteLine("Download Complete");
+            HttpClient client = new HttpClient();
+            string data = await client.GetStringAsync("http://rouxacademy.com");
+            Console.WriteLine("Download Complete" +data);
 
         }
     }
